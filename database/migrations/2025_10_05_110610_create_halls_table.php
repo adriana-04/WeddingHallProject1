e6e5d272c6f64f2b54f2b1b83aa7247549bc0c8d
Adawiyah Hashim <adrianaalia0404@gmail.com>
2025-10-05 19:25:29 +0800

    edit in halls table migration

--- database/migrations/2025_10_05_110610_create_halls_table.php
+++ database/migrations/2025_10_05_110610_create_halls_table.php
@@ -9,13 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('halls', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('halls', function (Blueprint $table) {
+        $table->id();
+        $table->string('hall_name');
+        $table->string('location');
+        $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
